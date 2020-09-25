66ecc5fe86c1c1bc1814a18df97f4df6e7e55743
Peter Allin <peter@peca.dk>
2020-09-26 01:13:45 +0200

    Add some tests

--- src/calculator.rs
+++ src/calculator.rs
@@ -19,3 +19,38 @@ impl Calculator {
         self.stack.iter().rev()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Calculator;
+
+    #[test]
+    fn starts_empty() {
+        let calc = Calculator::new();
+        assert_eq!(calc.stack_iter().count(), 0);
+    }
+
+    #[test]
+    fn push_two() {
+        let mut calc = Calculator::new();
+        calc.push("1".into());
+        calc.push("2".into());
+        let mut iter = calc.stack_iter();
+        assert_eq!("2", iter.next().unwrap());
+        assert_eq!("1", iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn push_drop() {
+        let mut calc = Calculator::new();
+        calc.push("1".into());
+        calc.push("2".into());
+        calc.drop();
+        calc.push("3".into());
+        let mut iter = calc.stack_iter();
+        assert_eq!("3", iter.next().unwrap());
+        assert_eq!("1", iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
+}
